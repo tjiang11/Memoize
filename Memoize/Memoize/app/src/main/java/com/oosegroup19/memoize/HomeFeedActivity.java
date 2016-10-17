2c27c5b34f6f7784b4b5b47ddaf4724d3e811c14
Sarah Sukardi <sukardi@jhu.edu>
2016-10-17 02:16:52 -0400

    refactor

---
+++ Memoize/Memoize/app/src/main/java/com/oosegroup19/memoize/HomeFeedActivity.java
@@ -0,0 +1,13 @@
+package com.oosegroup19.memoize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_feed);
+    }
+}
