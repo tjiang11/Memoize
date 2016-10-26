e3362025e023ea34d7f43e0bf2d02c0fb8e249eb
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2016-10-26 14:53:12 -0400

    moved pom

--- Memoize/Memoize/app/src/main/java/com/oosegroup19/memoize/HomePageActivity.java
+++ Memoize/Memoize/app/src/main/java/com/oosegroup19/memoize/HomePageActivity.java
@@ -31,7 +31,7 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_page_, menu);
+        //getMenuInflater().inflate(R.menu.menu_home_page_, menu);
         return true;
     }
 
