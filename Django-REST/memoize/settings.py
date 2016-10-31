6c3da6dee1ead52628aac8086e8d19f8fb5b7f2f
Tony Jiang <tjiang11@jhu.edu>
2016-10-31 00:27:41 -0400

    Migrate from Sqlite to Postgresql

--- Django-REST/memoize/settings.py
+++ Django-REST/memoize/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'memoize.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'memoize_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
