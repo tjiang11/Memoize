1480da36e1c113a73f0a6ef540acf383610e5ae4
Tony Jiang <tjiang11@jhu.edu>
2016-11-01 20:40:50 -0400

    Replace JSONResponse with rest_framework.Response. Fix bug with POST /Events

--- Django-REST/memoize/app/views.py
+++ Django-REST/memoize/app/views.py
@@ -4,19 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, views
+from rest_framework import viewsets, views, status
+from rest_framework.decorators import api_view
 from memoize.app.serializers import UserSerializer, GroupSerializer, EventSerializer
 from rest_framework.response import Response
 from memoize.app.models import Event
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -37,40 +30,38 @@ class TestView(views.APIView):
     def get(self, request, format=None):
         return Response("Hello REST World")
 
-
+@api_view(['GET', 'POST'])
 @csrf_exempt
 def event_list(request):
     if request.method == 'GET':
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = EventSerializer(data=data)
+        serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET', 'PUT', 'DELETE'])
 def event_detail(request, pk):
     try:
         event = Event.objects.get(pk=pk)
     except Event.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = EventSerializer(event)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = EventSerializer(event, data=data)
+        serializer = EventSerializer(event, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
+            return Response(serializer.data)
 
     elif request.method == 'DELETE':
         event.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
