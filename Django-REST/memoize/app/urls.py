e30d111b19eb809a590afb0a75a0d5278f4fd3b4
Tony Jiang <tjiang11@jhu.edu>
2016-11-01 21:01:16 -0400

    Convert event functional views to class-based views.

--- Django-REST/memoize/app/urls.py
+++ Django-REST/memoize/app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from memoize.app import views
 
 urlpatterns = [
-	url(r'^events/$', views.event_list),
-	url(r'^events/(?P<pk>[0-9]+)/$', views.event_detail),
+	url(r'^events/$', views.event_list.as_view()),
+	url(r'^events/(?P<pk>[0-9]+)/$', views.event_detail.as_view()),
 ]
--- Django-REST/memoize/app/views.py
+++ Django-REST/memoize/app/views.py
@@ -1,14 +1,11 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, views, status
-from rest_framework.decorators import api_view
-from memoize.app.serializers import UserSerializer, GroupSerializer, EventSerializer
 from rest_framework.response import Response
 from memoize.app.models import Event
+from memoize.app.serializers import UserSerializer, GroupSerializer, EventSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -26,42 +23,46 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 class TestView(views.APIView):
     def get(self, request, format=None):
         return Response("Hello REST World")
 
-@api_view(['GET', 'POST'])
-@csrf_exempt
-def event_list(request):
-    if request.method == 'GET':
+
+class event_list(views.APIView):
+    def get(self, request, format=None):
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def event_detail(request, pk):
-    try:
-        event = Event.objects.get(pk=pk)
-    except Event.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class event_detail(views.APIView):
+    def get_object(self, pk):
+        try:
+            return Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        event = self.get_object(pk)
         serializer = EventSerializer(event)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        event = self.get_object(pk)
         serializer = EventSerializer(event, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
         event.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
