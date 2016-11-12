c12a1948aa3abf2c1193d6ec7c1cb31720671e2d
Sarah Sukardi <sukardi@jhu.edu>
2016-11-12 14:17:53 -0500

    added tab layout to bottom and in process of making the tester stuff a fragment

--- Memoize/Memoize/app/src/main/java/com/oosegroup19/memoize/HomePageActivity.java
+++ Memoize/Memoize/app/src/main/java/com/oosegroup19/memoize/HomePageActivity.java
@@ -1,6 +1,5 @@
 package com.oosegroup19.memoize;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -11,29 +10,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 public class HomePageActivity extends AppCompatActivity {
-    private static final String DEBUG_TAG = "HttpExample";
-    private TextView textView;
-    private int PORT = 8000;
-    String test_url = "http://10.0.2.2:" + PORT + "/hello/";
-
-
-    //Sarah's variables for moving between fragments
-    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +23,13 @@ public class HomePageActivity extends AppCompatActivity {
         //remove app title from home page
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        textView = (TextView) findViewById(R.id.textView2);
+        FragmentManager fragmentManager = getFragmentManager();
 
-        testConnection();
-
-        //button listener
-        Button button1 = (Button) findViewById(R.id.test_button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectItem(1);
-            }
-        });
+        //replaces with lesson screen
+        fragmentManager.beginTransaction().replace(R.id.layout, new HomePageFragment()).commit();
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -79,101 +50,6 @@ public class HomePageActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    } */
 
-    public void testConnection() {
-        String stringUrl = test_url;
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new DownloadWebpageTask().execute(stringUrl);
-        } else {
-            textView.setText("No network connection available.");
-        }
-    }
-
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-            Log.d(DEBUG_TAG, "test log 1");
-            // params comes from the execute() call: params[0] is the url.
-            try {
-                return downloadUrl(urls[0]);
-            } catch (IOException e) {
-                Log.d(DEBUG_TAG, e.getMessage());
-                return "Unable to retrieve web page. URL may be invalid.";
-            }
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            textView.setText(result);
-        }
-    }
-
-    private String downloadUrl(String myurl) throws IOException {
-        InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
-        int len = 500;
-
-        try {
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            Log.d(DEBUG_TAG, "test log 2");
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            Log.d(DEBUG_TAG, "test log 3");
-            conn.connect();
-            Log.d(DEBUG_TAG, "test log 4");
-            int response = conn.getResponseCode();
-            Log.d(DEBUG_TAG, "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            return readIt(is, len);
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
-
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
-    }
-
-
-
-    private void selectItem(int position) {
-        // update the main content by replacing fragments
-        Fragment fragment = null;
-        switch (position) {
-            case 1:
-                mTitle = "New Lesson";
-                fragment = new ReminderLogFragment();
-                break;
-
-            default:
-                break;
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.layout, fragment).commit();
-        } else {
-            // error in creating fragment
-            Log.e("MainActivity", "Error in creating fragment");
-        }
-    }
 }
