eb96129150f8b1b2536c0b57d95f5db23106d488
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2016-11-13 20:07:14 -0500

    pydocs

--- Django-REST/memoize/app/models/Event.py
+++ Django-REST/memoize/app/models/Event.py
@@ -11,6 +11,15 @@ from django.db import models
 from Group import MemGroup
 
 class Event(models.Model):
+	"""Used when a user creates an event for a group.
+
+   	Args:
+   		start_time: when the event should start being tracked for.
+   		end_time: when the event should no longer be tracked for.
+   		location: where the event is taking place.
+   		tags: the tags associated with this event. 
+   		group: the group that this event belongs to.
+   	"""
 	name = models.CharField(max_length=255)
 	start_time = models.DateTimeField(auto_now_add=True)
 	end_time = models.DateTimeField(null=True)
--- Django-REST/memoize/app/models/User.py
+++ Django-REST/memoize/app/models/User.py
@@ -4,40 +4,102 @@ from Subscription import Subscription
 
 class User2(): 
 
-	def __init__(self, name, email, password):
-		self.name = name
-		self.email = email
-		self.password = password
-		self.location_reminders = []
-		self.time_reminders = []
-		self.my_groups = []
-		self.subscriptions = Subscription() 
-		self.admin = False
+  def __init__(self, name, email, password):
+    """Create a new User Object.
 
-		User.objects.create_user(username = name, email = email, password = password)
+    Args:
+        name: the name of the user.
+        email: email specified by the user.
+        password: password specified by the user.
+    """
+    self.name = name
+    self.email = email
+    self.password = password
+    self.location_reminders = []
+    self.time_reminders = []
+    self.my_groups = []
+    self.subscriptions = Subscription() 
+    self.admin = False
 
-	def login(self, email, password):
-		return True
+  #User.objects.create_user(username = name, email = email, password = password)
 
-	def create_l_reminder(self, info):
-		return True
+  def login(self, email, password):
+    """Allows used to login to his/her account.
 
-	def create_t_reminder(self, info):
-		return True
+     Args:
+     	  email: the email being used for the login.
+     	  password: the password being used for the login.   
+     Returns:
+      	True or False, depending on if the action is successful.
+     """
+    return True
 
-	def create_group(self, info):
-		self.admin = True
-		return True
+  def create_l_reminder(self, info):
+    """Allows user to create a location based reminder.
 
-	def create_group_event(self, group, info):
-		return True;
+     Args:
+     	  info: the info needed for the reminder.  
+     Returns:
+      	True or False, depending on if the action is successful.
+     """
+    return True
 
-	def subscribe_to_group(self, group):
-		subscriptions.subscribe_to_group(group)
+  def create_t_reminder(self, info):
+    """Allows user to create a time based reminder.
 
-	def subscribe_to_tag(self, tag):
-		subscriptions.subscribe_to_tag(tag)
+     Args:
+      	info: the info needed for the reminder.  
+     Returns:
+      	True or False, depending on if the action is successful.
+     """
+    return True
 
-	def return_info(self):
-		info = [self.name, self.email, self.password]
-		return info
+  def create_group(self, info):
+    """Allows user to create a group.
+
+     Args:
+      	info: the info needed for the group.  
+     Returns:
+     	  True or False, depending on if the action is successful.
+     """
+    self.admin = True
+    return True
+
+  def create_group_event(self, group, info):
+    """Allows user to create an event for a group.
+
+     Args:
+      	info: the info needed for the event.
+     	  group: the group associated with the event.   
+     Returns:
+     	  True or False, depending on if the action is successful.
+     """
+    return True;
+
+  def subscribe_to_group(self, group):
+    """Allows user to subscribe to a group.
+
+     Args:
+     	  group: the group the user is trying to subscribe to.  
+     Returns:
+      	True or False, depending on if the action is successful.
+     """
+    subscriptions.subscribe_to_group(group)
+
+  def subscribe_to_tag(self, tag):
+    """Allows user to subscribe to a tag.
+
+     Args:
+      	tag: the tag the user is trying to subscribe to.  
+     Returns:
+      	True or False, depending on if the action is successful.
+     """
+    subscriptions.subscribe_to_tag(tag)
+
+  def return_info(self):
+    """Returns the info of a user. 
+     Returns:
+      	the info of the user. 
+     """
+    info = [self.name, self.email, self.password]
+    return info
