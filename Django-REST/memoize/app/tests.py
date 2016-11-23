c7feede54a1ca13a39dcd6ce25a6b44cfe3d1ea1
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2016-11-23 02:15:47 -0500

    6 happy path, 4 unhappy path

--- Django-REST/memoize/app/tests.py
+++ Django-REST/memoize/app/tests.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from django.test import TestCase
 from rest_framework.test import APITestCase
 import unittest
-import models
+#import models
 from rest_framework.test import APIClient
 
 client = APIClient()
@@ -12,45 +12,57 @@ count = 0
 
 # Create your tests here.
 class happy_path_tests(APITestCase):
+	"""TEST SUITE (for happy path tests)."""
 	
 	def test_creating_user(self):
+		"""Tests the basic creation of a user (successful creation)."""
 		response = make_test_user(self)
+		#Test response
+		self.assertEquals(response.content, '{"id":3,"username":"test0@jhu.edu","mem_groups":[],"sub_groups":[],"location_reminders":[],"time_reminders":[]}')
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-
-		#data2 = {"username": "test2@jhu.edu2", "password": "PaSsWoRd2",  "mem_groups": [], "sub_groups": []}
-		#response = self.client.post('/users/', data2, format='json')
+		#Test that user is actually in database
+		response = self.client.get("/users/3/")
+		self.assertEquals(response.content, '{"id":3,"username":"test0@jhu.edu","mem_groups":[],"sub_groups":[]}')
+		#Test response
 		response = make_test_user(self)
+		self.assertEquals(response.content, '{"id":4,"username":"test1@jhu.edu","mem_groups":[],"sub_groups":[],"location_reminders":[],"time_reminders":[]}')
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		#Test that user is actually in database
+		response = self.client.get("/users/4/")
+		self.assertEquals(response.content, '{"id":4,"username":"test1@jhu.edu","mem_groups":[],"sub_groups":[]}')
 
-		#client.login(username='test1@jhu.edu', password='PaSsWoRd1')
-		#reponse = self.client.get('/users/', format='json')
-		#print reponse
-		#print response.status_code
-		#data2 = {'password': 'PaSsWoRd'}
-		#response = self.client.post('/users/1', data2, format='json')
-		#print response.status_code
 		
 
 	def test_time_reminder(self):	
-		#data = {"username": "test3@jhu.edu", "password": "PaSsWoRd3",  "mem_groups": [], "sub_groups": []}
-		#response = self.client.post('/users/', data, format='json')
+		"""Tests successful creation of a time reminder."""
 		response = make_test_user(self)
-		data3 = {"time": "1996-12-05T06:32:00", "name": "buy food", "description": "descripto patronum", "location_descriptor": "Hogwarts school of oose"}
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		data3 = {"time": "1996-12-05T06:32:00", "name": "make a time reminder", "description": "this is a test description", "location_descriptor": "TEST"}
 		response = self.client.post('/users/6/timereminders/', data3, format='json')
-
+		self.assertEquals(response.content, '{"name":"make a time reminder","description":"this is a test description","location_descriptor":"TEST","time":"1996-12-05T06:32:00Z"}')
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		
+		#test if actually stored
 		response = self.client.get('/users/6/timereminders/', {}, format = 'json')
-		self.assertEqual(response.content, '[{"name":"buy food","description":"descripto patronum","location_descriptor":"Hogwarts school of oose","time":"1996-12-05T06:32:00Z"}]')
+		self.assertEqual(response.content, '[{"name":"make a time reminder","description":"this is a test description","location_descriptor":"TEST","time":"1996-12-05T06:32:00Z"}]')
 		self.assertEqual(response.status_code, 200)
 
 	def test_location_reminder(self):
+		"""Tests successful creation of a location reminder."""
 		response = make_test_user(self)
-		data = {"start_time": "10:45[:0[0]]", "name": "buy food", "description": "descripto patronum", "location_descriptor": "Hogwarts school of oose", "end_time": "11:45[:0[0]]", "latitude": "1.00", "longitude": "1.00"}
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		data = {"start_time": "10:45[:0[0]]", "name": "make a location reminder", "description": "this is a test description", "location_descriptor": "test location", "end_time": "11:45[:0[0]]", "latitude": "1.00", "longitude": "1.00"}
 		response = self.client.post('/users/5/locationreminders/', data, format='json')
-		self.assertEqual(response.content, '{"name":"buy food","description":"descripto patronum","location_descriptor":"Hogwarts school of oose","start_time":"10:45:00","end_time":"11:45:00","latitude":"1.00000","longitude":"1.00000"}')
-		self.assertEqual(response.status_code, 201) 
+		self.assertEqual(response.content, '{"name":"make a location reminder","description":"this is a test description","location_descriptor":"test location","start_time":"10:45:00","end_time":"11:45:00","latitude":"1.00000","longitude":"1.00000"}')
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED) 
+
+		#test if actually stored
+		response = self.client.get('/users/5/locationreminders/', {}, format = 'json')
+		self.assertEqual(response.content, '[{"name":"make a location reminder","description":"this is a test description","location_descriptor":"test location","start_time":"10:45:00","end_time":"11:45:00","latitude":"1.00000","longitude":"1.00000"}]')
+		self.assertEqual(response.status_code, 200)
 
 	def test_group(self): #will need to be modified when we add authentication!
+		"""Tests successful creation of a group."""
 		data = {
 		    "name": "test group",
 		    "description": "this is a test group",
@@ -59,12 +71,13 @@ class happy_path_tests(APITestCase):
 		    "events": []
 		}
 		response = self.client.post('/groups/', data, format='json')
-		self.assertEqual(response.status_code, 201)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 		self.assertEqual(response.content, '{"id":3,"name":"test group","description":"this is a test group","admins":[],"subscribers":[],"events":[]}')
 
 
 	#things to consider: when making a group I am not in the admins list for that group. Should probably be fixed
 	def test_adding_subscription_to_group(self):
+		"""Tests successful creation of a subsrcription to a group."""
 		data = {"username": "test_subscription@jhu.edu", "password": "PaSsWoRdTest",  "mem_groups": [], "sub_groups": []}
 		response = self.client.post('/users/', data, format='json')	
 		data = {
@@ -75,14 +88,22 @@ class happy_path_tests(APITestCase):
 		    "events": []
 		}
 		response = self.client.post('/groups/', data, format='json')
-		self.assertEqual(response.status_code, 201)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 		self.assertEqual(response.content, '{"id":2,"name":"group that should be added for subscription testing","description":"this is a test group","admins":[],"subscribers":[2],"events":[]}')
 
 		response = self.client.get('/users/2/', {}, format='json')
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.content, '{"id":2,"username":"test_subscription@jhu.edu","mem_groups":[],"sub_groups":[2]}')
 
+		response = self.client.get('/users/2/subgroups/')
+		self.assertEquals(response.content, '[{"id":2,"name":"group that should be added for subscription testing","description":"this is a test group","admins":[],"subscribers":[2],"events":[]}]')	
+
+		response.client.get('/users/2/subgroups/1/')
+		self.assertEquals(response.content, '[{"id":2,"name":"group that should be added for subscription testing","description":"this is a test group","admins":[],"subscribers":[2],"events":[]}]')
+
 	def test_adding_event_to_group(self):
+		"""Tests successful creation of an event."""
+		"""Tests successful creation of a time reminder."""
 		data = {"username": "test_subscription@jhu.edu", "password": "PaSsWoRdTest",  "mem_groups": [], "sub_groups": []}
 		response = self.client.post('/users/', data, format='json')	
 		data = {
@@ -93,7 +114,7 @@ class happy_path_tests(APITestCase):
 		    "events": []
 		}
 		response = self.client.post('/groups/', data, format='json')
-		self.assertEqual(response.status_code, 201)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 		self.assertEqual(response.content, '{"id":1,"name":"group that should be added for subscription testing","description":"this is a test group","admins":[],"subscribers":[1],"events":[]}')
 
 		response = self.client.get('/users/1/', {}, format='json')
@@ -123,21 +144,38 @@ class happy_path_tests(APITestCase):
 		self.assertEqual(response.content, '{"name":"This is a test event for group 1","description":"This is a test description","location_descriptor":"This is a test location","start_time":"1996-12-05T06:32:00Z","end_time":"1996-12-05T06:32:00Z","longitude":"1.00000","latitude":"1.00000","tags":"test tag","group":1}')
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+		response = self.client.get('/events/', {}, format='json')
+		self.assertEquals(response.content, '[{"name":"This is a test event for group 1","description":"This is a test description","location_descriptor":"This is a test location","start_time":"1996-12-05T06:32:00Z","end_time":"1996-12-05T06:32:00Z","longitude":"1.00000","latitude":"1.00000","tags":"test tag","group":1}]')
 
-class non_happy_path_tests(APITestCase):
+		response = self.client.get('/events/1/', {}, format='json')
+		self.assertEquals(response.content, '{"name":"This is a test event for group 1","description":"This is a test description","location_descriptor":"This is a test location","start_time":"1996-12-05T06:32:00Z","end_time":"1996-12-05T06:32:00Z","longitude":"1.00000","latitude":"1.00000","tags":"test tag","group":1}')
+
+
+class unhappy_path_tests(APITestCase):
+	"""TEST SUITE (for unhappy path tests)"""
 	def test_making_user_without_username_password(self):
-		#test creating a user without an email and without a password (name? I don't remember)
+		"""Tests that creating au user wothout a username or a password fails."""
+
+		#test creating a user without a username (email)
 		data = {"username": "", "password": "PaSsWoRd2",  "mem_groups": [], "sub_groups": []}
 		response = self.client.post('/users/', data, format='json')
 		self.assertEquals(response.content, '{"username":["This field may not be blank."]}')
 		self.assertEquals(response.status_code, 400)
 
+		#test creating a user without a password
 		data2 = {"username": "test_without_password", "password": "",  "mem_groups": [], "sub_groups": []}
 		response = self.client.post('/users/', data2, format='json')
 		self.assertEquals(response.content, '{"password":["This field may not be blank."]}')
 		self.assertEquals(response.status_code, 400)
 
+		#test creating a user without a username (email) or password
+		data3 = {"username": "", "password": "",  "mem_groups": [], "sub_groups": []}
+		response = self.client.post('/users/', data, format='json')
+		self.assertEquals(response.content, '{"username":["This field may not be blank."]}') #so we may want to change this 
+		self.assertEquals(response.status_code, 400)
+
 	def test_reminders_without_descriptions(self):
+		"""Tests that creating a reminder without a description fails."""
 		response = make_test_user(self)
 		data3 = {"time": "1996-12-05T06:32:00", "name": "buy food", "description": "", "location_descriptor": "Hogwarts school of oose"}
 		response = self.client.post('/users/7/timereminders/', data3, format='json')
@@ -153,12 +191,8 @@ class non_happy_path_tests(APITestCase):
 		response = self.client.get('/users/7/locationreminders/', {}, format = 'json')
 		self.assertEquals(response.content, '[]')
 
-		#test making a user with incorrect credentials (do we have this implemented yet?)
-
-		#test making reminder (both) without a description (this might be a happy path thing actually)
-
-		#test making group without name (should not work)
 	def test_z_making_group_without_name_and_description(self):
+		"""Test that making a group without a name or description or both fails."""
 		data = {
 		    "name": "",
 		    "description": "this is a test group",
@@ -179,8 +213,18 @@ class non_happy_path_tests(APITestCase):
 		response = self.client.post('/groups/', data, format='json')
 		self.assertEquals(response.content, '{"description":["This field may not be blank."]}')
 
+		data = {
+		    "name": "",
+		    "description": "",
+		    "admins": [],
+		    "subscribers": [],
+		    "events": []
+		}
+		response = self.client.post('/groups/', data, format='json')
+		self.assertEquals(response.content, '{"name":["This field may not be blank."],"description":["This field may not be blank."]}')
 
 	def test_z_reminders_without_time(self):
+		"""Test that making a time reminder without a time fails."""
 		response = make_test_user(self)
 		data = {"time": "", "name": "test without time", "description": "we are testing", "location_descriptor": "Hogwarts school of oose"}
 		response = self.client.post('/users/8/timereminders/', data, format='json')
@@ -195,29 +239,13 @@ class non_happy_path_tests(APITestCase):
 		self.assertEquals(response.content, '{"end_time":["Time has wrong format. Use one of these formats instead: hh:mm[:ss[.uuuuuu]]."]}')
 
 
-		#test making time reminder without a time (will fail)
-
-
-
-
-
-
-
-
-	
-
 def make_test_user(self):
+	"""Method to make a user in the database that is used by various other methods."""
 	global count
 	data = {"username": "test" + str(count) + "@jhu.edu", "password": "PaSsWoRd" + str(count),  "mem_groups": [], "sub_groups": []}
 	response = self.client.post('/users/', data, format='json')	
 	count = count + 1
 	return response
 
-
-def main():
-    unittest.main()
-
-if __name__ == '__main__':
-    main() 
 
 	
