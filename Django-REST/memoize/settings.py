54803e8ec2ad8b764127048b7e76b50c2a73beaf
Tony Jiang <tjiang11@jhu.edu>
2016-12-21 18:49:57 -0500

    update static file setup

--- Django-REST/memoize/settings.py
+++ Django-REST/memoize/settings.py
@@ -154,9 +154,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR + STATIC_URL
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 REST_FRAMEWORK = {
     #'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
